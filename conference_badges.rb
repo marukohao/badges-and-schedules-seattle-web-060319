bdc23d97df0f1fbf34105705f6d9cc0c499396f8
hao wang <ge_nah@hotmail.com>
2019-05-09 19:50:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,7 +14,7 @@ end
 def assign_rooms(arr)
   new_arr = []
   arr.each_with_index do |e, i|
-    new_arr << "Hello, #{e}!You'll be assigned to room #{i + 1}!"
+    new_arr << "Hello, #{e}! You'll be assigned to room #{i + 1}!"
   end
   new_arr
 end
