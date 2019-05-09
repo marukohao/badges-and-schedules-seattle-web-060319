e1f12133ed82b320f8301a337e29f35e9d8c06dc
hao wang <ge_nah@hotmail.com>
2019-05-09 19:39:19 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,14 @@
 # Write your code here.
+def badge_maker(name)
+  "Hello, my name is #{}."
+end
+
+def batch_badge_creator(arr)
+  arr
+end
+
+def assign_rooms(arr)
+  arr.each_with_index do |e, i|
+    "Hello, #{e}!You'll be assigned to room #{i + 1}!"
+  end
+end
